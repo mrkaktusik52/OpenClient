404922b1c18c7899311496e30550d91c027b97c6
mrkaktusik52 <commonx282@gmail.com>
2026-08-14 21:07:55 +0300

    feat: added victim's armor to target hud

--- src/main/java/com/cactus/hud/modules/TargetHud.java
+++ src/main/java/com/cactus/hud/modules/TargetHud.java
@@ -7,8 +7,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.HitResult;
 
 
@@ -41,10 +43,18 @@ public class TargetHud extends HudModule {
 
         float hp = 0.0f;
         float maxHp = 0.0f;
+        ItemStack helmet= ItemStack.EMPTY;
+        ItemStack chest = ItemStack.EMPTY;
+        ItemStack legs = ItemStack.EMPTY;
+        ItemStack feet = ItemStack.EMPTY;
 
         if (lastHit instanceof LivingEntity living) {
             hp = living.getHealth();
             maxHp = living.getMaxHealth();
+            helmet = living.getItemBySlot(EquipmentSlot.HEAD);
+            chest = living.getItemBySlot(EquipmentSlot.CHEST);
+            legs = living.getItemBySlot(EquipmentSlot.LEGS);
+            feet = living.getItemBySlot(EquipmentSlot.FEET);
         }
 
         if (lastHit instanceof Player player) {
@@ -52,12 +62,18 @@ public class TargetHud extends HudModule {
 
         }
 
+
+
         this.setWidth(Math.max(mc.font.width(name), mc.font.width(hp + " / " + maxHp)) + 10);
-        setHeight(30);
+        setHeight(45);
 
         graphics.fill(getX(), getY(), getX() + getWidth(), getY() + getHeight(), 0x80000000);
         graphics.drawString(mc.font, name, getX() + 5, getY() + 5, 0xFFFFFFFF);
         graphics.drawString(mc.font, hp + " / " + maxHp, getX() + 5, getY() + 15, 0xFFFFFFFF);
+        graphics.renderItem(helmet, getX() + 2, getY() + 25);
+        graphics.renderItem(chest, getX() + 17, getY() + 25);
+        graphics.renderItem(legs, getX() + 32, getY() + 25);
+        graphics.renderItem(feet, getX() + 47, getY() + 25);
     }
     //    public Identifier getIcon() {
 //        return Identifier.fromNamespaceAndPath(OpenClient.MOD_ID, "textures/gui/logo-transperent.png");
